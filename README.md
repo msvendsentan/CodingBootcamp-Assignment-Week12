a1ee34447c9325debf49cb326a33fd837806b2f0
unknown <msvendsentan@gmail.com>
2018-08-18 17:02:58 -0400

    first set

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CodingBootcamp-Assignment-Week12
 A simple inventory management command line tool for Week 12 of UofT's Coding Bootcamp
+
+**Instructions**
+* This is a node application that requires the inquirer and mysql packages; do not forget to install after cloning!
+* You are using your command line to navigate a simple "storefront"
+* Just follow the prompts!
+
+**Specifications**
+* Use node, inquirer, and mysql to build a persistant marketplace where users can scan, inspect, and purchase items, and their purchases affect the persistent stock
+
+**Features**
+
+**Future Directions**
+* Aesthetics (in code and console!)
+* Data validation--currently users can enter in all sorts of things when purchasing
+* Manager & supervisor functionality as described in the assignment instructions
+  * More robust queries to view low inventory, department costs, etc
+  * Ability to add / remove products from marketplace, create new departments, etc
--- bamazonCusomter.js
+++ bamazonCusomter.js
@@ -9,6 +9,168 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
-connection.query("SELECT * FROM products", function(err, res) {
-    console.log(res);
-})
+var itemArr = [];
+
+var options = {
+    "See all items": function() {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            res.forEach(function(element) {
+                console.log(element.product_name);
+            });
+            homeMenu();
+        });
+    },
+    "Get more information on an item": function() {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            res.forEach(function(element) {
+                itemArr.push(element.product_name);
+            });
+            itemInfo();
+        });       
+    },
+    "Purchase an item": function() {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            res.forEach(function(element) {
+                itemArr.push(element.product_name);
+            });
+            itemPurchase();
+        });
+    },
+    "Terminate session": function() {
+        console.log("Thanks for using bamazon!")
+        connection.end();
+    }
+}
+
+function homeMenu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                choices: [
+                    "See all items",
+                    "Get more information on an item",
+                    "Purchase an item",
+                    "Terminate session"
+                ],
+                name: "customeroptions"
+            }
+        ]).then(function(answers) {
+            options[answers.customeroptions]();
+        });
+}
+
+function itemInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Which item would you like more information about?",
+                choices: itemArr,
+                name: "iteminfoselect"
+            }
+        ]).then(function(answers) {
+            connection.query(
+                "SELECT * FROM products WHERE ?",
+                {
+                    product_name: answers.iteminfoselect
+                },
+                function(err, res) {
+                    // Stringify & parse removes annoying "RowDataPacket" text
+                    console.log(JSON.parse(JSON.stringify(res)));
+                    homeMenu();
+                }
+            );
+        });
+}
+
+function itemPurchase() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Which item would you like to purchase?",
+                choices: itemArr,
+                name: "itempurchaseselect"
+            },
+            {
+                type: "input",
+                message: "How many would you like to purchase?",
+                name: "quantity"
+            }
+        ]).then(function(answers) {
+            connection.query(
+                "SELECT * FROM products WHERE ?",
+                {
+                    product_name: answers.itempurchaseselect
+                },
+                function(err, res) {
+                    console.log(res[0].product_name);
+                    if (answers.quantity > res[0].stock_quantity) {
+                        console.log("We're sorry, we don't have that amount in stock!");
+                        homeMenu();
+                    } else {
+                        console.log("Purchasing " + answers.quantity + " of " + res[0].product_name + " will cost $" + (res[0].price * answers.quantity) + " dollars.");
+                        updateStock(res[0].item_id, res[0].stock_quantity, answers.quantity);
+                    }
+                }
+            );
+        });
+}
+
+function updateStock(id, oldamount, purchaseamount) {
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: oldamount - purchaseamount
+            },
+            {
+                item_id: id
+            }
+        ],
+        function(err, res) {
+            console.log("Transaction successfully completed!");
+            homeMenu();
+        }
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function readProductNames() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        res.forEach(function(element) {
+            console.log(element.product_name);
+        });
+    });
+}
+
+function readProduct() {
+    connection.query("SELECT * FROM products", function(err, res) {
+
+        if (err) throw err;
+
+        
+        console.log(JSON.parse(JSON.stringify(res)));
+    });
+}
+
+connection.connect(function(err) {
+    if (err) throw err;
+    homeMenu();
+});
